3b8607b6de459c1c008a3e1122b66fa842164bb2
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-12-15 21:26:06 +0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "images.pexels.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
